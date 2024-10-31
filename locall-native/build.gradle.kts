d8be2245af5931cadd798659ba5b4b595b5b4920
GMerge <gemingjia0201@163.com>
2024-10-31 23:33:55 +0800

    feat: delete .idea

--- locall-native/build.gradle.kts
+++ locall-native/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    kotlin("jvm") version "2.0.21"
+    kotlin("jvm")
 }
 
 group = "org.locallback"
@@ -11,20 +11,12 @@ repositories {
 }
 
 dependencies {
-    implementation(project(":locall-annotation"))
     implementation(project(":locall-common"))
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation(kotlin("stdlib-jdk8"))
 }
-
-kotlin {
-    jvmToolchain(21)
-}
-
-
-
 
 tasks.test {
     useJUnitPlatform()
