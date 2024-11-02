a84dd54ac14f13f562cfa8d011db82cd12508969
GMerge <gemingjia0201@163.com>
2024-11-02 23:32:30 +0800

    feat: add availableMethodMap

--- locall-framework/src/main/java/org/locallback/framework/LocallContext.java
+++ locall-framework/src/main/java/org/locallback/framework/LocallContext.java
@@ -2,23 +2,49 @@ package org.locallback.framework;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LocallContext {
 
     private static volatile LocallContext INSTANCE = null;
 
-    /**
-     * 可用方法列表
-     */
-    private List<Method> availableMethodList = new ArrayList<>();
+    /* 可用方法列表 */
+    private final List<Method> availableMethodList = new ArrayList<>();
 
-    public List<Method> getAvailableMethodList() {
+    private final Map<String, Method> availableMethodMap = new HashMap<>();
+
+    protected List<Method> getAvailableMethodList() {
         return availableMethodList;
     }
 
     public void setAvailableMethodList(List<Method> availableMethodList) {
-        this.availableMethodList = availableMethodList;
+        availableMethodList.forEach(this::addAvailableMethod);
+    }
+
+    /**
+     * 将可用方法列表转换为Map，可通过方法名获取方法
+     * 为保证availableMethodList和availableMethodMap的一致性，故不提供Map的set方法
+     *
+     * @param method AvailableMethod
+     */
+    public void addAvailableMethod(Method method) {
+        synchronized (this) {
+            availableMethodList.add(method);
+            availableMethodMap.put(method.getName(), method);
+        }
+
+    }
+
+    /**
+     * 通过方法名获取可用方法
+     *
+     * @param methodName 方法名
+     * @return Method
+     */
+    public Method getAvailableMethod(String methodName) {
+        return availableMethodMap.get(methodName);
     }
 
     private LocallContext() {
