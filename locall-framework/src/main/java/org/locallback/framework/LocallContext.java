667939cc3d64bb447aef452a9fd75241ce89f2e2
GMerge <gemingjia0201@163.com>
2024-11-03 23:20:15 +0800

    opt: access control character

--- locall-framework/src/main/java/org/locallback/framework/LocallContext.java
+++ locall-framework/src/main/java/org/locallback/framework/LocallContext.java
@@ -10,43 +10,6 @@ public class LocallContext {
 
     private static volatile LocallContext INSTANCE = null;
 
-    /* 可用方法列表 */
-    private final List<Method> availableMethodList = new ArrayList<>();
-
-    private final Map<String, Method> availableMethodMap = new HashMap<>();
-
-    protected List<Method> getAvailableMethodList() {
-        return availableMethodList;
-    }
-
-    public void setAvailableMethodList(List<Method> availableMethodList) {
-        availableMethodList.forEach(this::addAvailableMethod);
-    }
-
-    /**
-     * 将可用方法列表转换为Map，可通过方法名获取方法
-     * 为保证availableMethodList和availableMethodMap的一致性，故不提供Map的set方法
-     *
-     * @param method AvailableMethod
-     */
-    public void addAvailableMethod(Method method) {
-        synchronized (this) {
-            availableMethodList.add(method);
-            availableMethodMap.put(method.getName(), method);
-        }
-
-    }
-
-    /**
-     * 通过方法名获取可用方法
-     *
-     * @param methodName 方法名
-     * @return Method
-     */
-    public Method getAvailableMethod(String methodName) {
-        return availableMethodMap.get(methodName);
-    }
-
     private LocallContext() {
     }
 
@@ -60,4 +23,42 @@ public class LocallContext {
         }
         return INSTANCE;
     }
+
+    /* 可用方法列表 */
+    private final List<Method> availableMethodList = new ArrayList<>();
+
+    private final Map<String, Method> availableMethodMap = new HashMap<>();
+
+    protected List<Method> getAvailableMethodList() {
+        return availableMethodList;
+    }
+
+    protected void setAvailableMethodList(List<Method> availableMethodList) {
+        availableMethodList.forEach(this::addAvailableMethod);
+    }
+
+    /**
+     * 将可用方法列表转换为Map，可通过方法名获取方法
+     * 为保证availableMethodList和availableMethodMap的一致性，故不提供Map的set方法
+     *
+     * @param method AvailableMethod
+     */
+    protected void addAvailableMethod(Method method) {
+        synchronized (this) {
+            availableMethodList.add(method);
+            availableMethodMap.put(method.getName(), method);
+        }
+
+    }
+
+    /**
+     * 通过方法名获取可用方法
+     *
+     * @param fullMethodName 完整方法名 包名.类名#方法名
+     * @return Method
+     */
+    public Method getAvailableMethod(String fullMethodName) {
+        return availableMethodMap.get(fullMethodName);
+    }
+
 }
