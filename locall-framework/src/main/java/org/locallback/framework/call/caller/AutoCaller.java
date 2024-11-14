dc41ae3502a434e892bafffca307be84a03f1902
GMerge <gemingjia0201@163.com>
2024-11-14 23:01:08 +0800

    opt: opt AutoCaller code style

--- locall-framework/src/main/java/org/locallback/framework/call/caller/AutoCaller.java
+++ locall-framework/src/main/java/org/locallback/framework/call/caller/AutoCaller.java
@@ -10,15 +10,16 @@ public class AutoCaller<T> implements Caller<T> {
 
     private final LocallContext locallContext = LocallContext.getContext();
 
+    Caller<T> caller = null;
+
     @Override
     public T call(String methodName, Object... args) {
-        if (locallContext.getMethod(methodName) != null) {
-            Caller<T> localCaller = new LocalCaller<>();
-            return localCaller.call(methodName, args);
+        if ((locallContext.getMethod(methodName) != null)) {
+            caller = new LocalCaller<>();
         } else {
-            Caller<T> remoteCaller = new RemoteCaller<>();
-            return remoteCaller.call(methodName, args);
+            caller = new RemoteCaller<>();
         }
+        return caller.call(methodName, args);
     }
 
 }
